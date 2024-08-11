626e46a846b9915d742e551cc1c1ccfd6c4b382e
sonac <sonag007@gmail.com>
2024-08-11 07:47:09 +0200

    chore: adding some error handling

--- app/server/server.go
+++ app/server/server.go
@@ -211,7 +211,7 @@ func (h *HttpHandler) webhookActivity(w http.ResponseWriter, r *http.Request) {
 		usr.StravaAccessToken = authData.AccessToken
 	}
 
-	fmt.Printf("%+v", wBody)
+	slog.Info("got webhookBody" + fmt.Sprintf("%+v", wBody))
 
 	activity, err := strava.GetActivity(usr.StravaAccessToken, wBody.ObjectId)
 	if err != nil {
@@ -220,7 +220,8 @@ func (h *HttpHandler) webhookActivity(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("%+v", activity)
+	slog.Info("got webhookBody" + fmt.Sprintf("%+v", activity))
+
 	dbUsr, err := h.DB.GetUserByStravaId(wBody.OwnerId)
 	if err != nil {
 		slog.Error("error when fetching user from DB", "err", err.Error())
